4c00f25da59437e2aa889b4653ab9c5f4bb48974
Wissididom <webmaster@wissididom.me>
2022-05-03 22:27:07 +0200

    Added try, catch, async, await for deletemessage

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ client.connect().then(([server, port]) => {
 }).catch(err => {
     console.error(`Error connecting to Twitch Chat: ${err}`);
 });
-client.on('message', (channel, tags, message, self) => {
+client.on('message', async (channel, tags, message, self) => {
     if (self) return; // Ignore echoed messages.
     if (message.toLowerCase() === '!antifollowerping') {
         client.say(channel, 'antifollowerpong');
@@ -26,8 +26,12 @@ client.on('message', (channel, tags, message, self) => {
         // Example message: "Buy viewers, followers and primes on website. com"
         // Example message: "Wanna become famous? Buy viewers, followers and primes on website. shop "
         if (message.match(/.*Buy (?:(?:viewers|followers|primes|and),? ?)+ on .+\. ?(?:com|shop)/gi)) {
-            client.deletemessage(channel, tags.id);
-            console.log(`Deleted Message by ${tags.username}: ${message}`);
+			try {
+            	await client.deletemessage(channel, tags.id);
+            	console.log(`Deleted Message by ${tags.username}: ${message}`);
+            } catch (err) {
+            	console.log(`Couldn't delete Message due to: ${err}`);
+            }
         } else {
             console.log(`Not Bypassed by Badges of ${tags.username}: ${message}`);
         }
